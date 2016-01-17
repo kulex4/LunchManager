62e5c59fdfacf7cb0afe42f7473b12e163aa9cbf
Nikolai Pashkevich <kulex4@gmail.com>
2016-01-17 17:14:24 +0300

    spring security added

--- src/test/java/by/npashkevich/LunchManagerApplicationTests.java
+++ src/test/java/by/npashkevich/LunchManagerApplicationTests.java
@@ -2,11 +2,13 @@ package by.npashkevich;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = LunchManagerApplication.class)
+@WebAppConfiguration
 public class LunchManagerApplicationTests {
 
 	@Test
