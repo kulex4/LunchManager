4ebae7d2d646b6100149f83165fe17b2b4e7410d
Nikolai Pashkevich <kulex4@gmail.com>
2016-01-17 16:59:13 +0300

    initial commit, sample project generated in spring initializr

---
+++ src/main/java/by/npashkevich/LunchManagerApplication.java
@@ -0,0 +1,12 @@
+package by.npashkevich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchManagerApplication.class, args);
+	}
+}
